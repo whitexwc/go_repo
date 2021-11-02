dd3b0fe0a928b6da25cca0a3d6b2524e32dffd93
wucheng xiao <thinker55@163.com>
2021-11-02 15:31:46 +0800

    Create docu_example.go

---
+++ docu_example.go
@@ -0,0 +1,13 @@
+//here is the description of the docu_example
+package docu_example
+
+//here the func docu, just return the string passed in
+func docu(s string) string{
+  return s
+}
+
+//here is the doc type
+type doc struct{
+  ty string
+  b bool
+}
